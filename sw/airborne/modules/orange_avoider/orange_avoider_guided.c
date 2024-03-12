7f1c65a45bbc45d59b56ffc516ec2bd5088b6ed6
MerlijnBroekers2 <105420709+MerlijnBroekers2@users.noreply.github.com>
2024-03-12 17:36:23 +0100

    sdfgdfsg

--- sw/airborne/modules/orange_avoider/orange_avoider_guided.c
+++ sw/airborne/modules/orange_avoider/orange_avoider_guided.c
@@ -156,6 +156,8 @@ void orange_avoider_guided_periodic(void)
   VERBOSE_PRINT("Floor count: %d, threshold: %d\n", floor_count, floor_count_threshold);
   VERBOSE_PRINT("Floor centroid: %f\n", floor_centroid_frac);
 
+  PRINT("TOTAL_DIV %f", total_divergence);
+
   // update our safe confidence using color threshold
   if(color_count < color_count_threshold){
     obstacle_free_confidence++;
