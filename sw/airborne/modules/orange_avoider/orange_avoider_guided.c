216ecb4e6a004d1049b63be193524105fe3dfe9a
loerlemans <lars.00@live.nl>
2024-03-15 10:43:50 +0100

    d

--- sw/airborne/modules/orange_avoider/orange_avoider_guided.c
+++ sw/airborne/modules/orange_avoider/orange_avoider_guided.c
@@ -58,7 +58,7 @@ enum navigation_state_t {
 float oag_color_count_frac = 0.18f;       // obstacle detection threshold as a fraction of total of image
 float oag_floor_count_frac = 0.05f;       // floor detection threshold as a fraction of total of image
 float oag_max_speed = 0.5f;               // max flight speed [m/s]
-float oag_heading_rate = RadOfDeg(10.f);  // heading change setpoint for avoidance [rad/s]
+float oag_heading_rate = RadOfDeg(20.f);  // heading change setpoint for avoidance [rad/s]
 
 // define and initialise global variables
 enum navigation_state_t navigation_state = SEARCH_FOR_SAFE_HEADING;   // current state in state machine
